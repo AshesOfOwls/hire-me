d54c8579b26de3c2a2da54c4b42da1c19735a3c6
Quinn Elwood <ashes@quinns-mbp.lan>
2021-04-08 17:21:30 -0700

    Get storybook working with CRA

---
+++ src/App/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import s from './App.module.css';
+
+function App() {
+  return (
+    <div className={s.app}>
+      This is the app
+    </div>
+  );
+}
+
+export default App;
